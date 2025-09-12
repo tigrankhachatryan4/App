3d929ba78b4bb57db4a621392ffac72adb1c4946
tigrankhachatryan4 <tigran4484@yahoo.com>
2025-09-12 10:05:25 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,13 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('headache-diary-v1').then(cache => {
+      return cache.addAll(['./', './manifest.json', './icon-192.png', './icon-512.png']);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
