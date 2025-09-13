7709545f20fb59483ec70d8c1b1c3b74a6c43c2a
tigrankhachatryan4 <tigran4484@yahoo.com>
2025-09-12 18:53:48 -0700

    sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,27 @@
-self.addEventListener('install', e => {
+const CACHE = "headache-diary-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./icon-180.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (e) => {
   e.waitUntil(
-    caches.open('headache-diary-v1').then(cache => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './manifest.json',
-        './icon-192.png',
-        './icon-512.png'
-      ]);
-    })
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
   );
 });
 
-self.addEventListener('fetch', e => {
+self.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
